8fdfa4ec0222afe68d7194c21e2759cb39d4fa78
Тарасов Илья <ilyatarasow2005@gmail.com>
2026-02-24 12:59:14 +0300

    Added try-with-resources for context closing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,24 @@
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean1 =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean1.getMessage());
-        HelloWorld bean2 =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean2.getMessage());
+        try (AnnotationConfigApplicationContext applicationContext =
+                     new AnnotationConfigApplicationContext(AppConfig.class)) {
 
-        Cat cat1 =
-                (Cat) applicationContext.getBean("cat");
-        Cat cat2 = applicationContext.getBean("cat", Cat.class);
-        System.out.println(bean1 == bean2);
-        System.out.println(cat1 == cat2);
 
+            HelloWorld bean1 =
+                    (HelloWorld) applicationContext.getBean("helloworld");
+            System.out.println(bean1.getMessage());
+            HelloWorld bean2 =
+                    (HelloWorld) applicationContext.getBean("helloworld");
+            System.out.println(bean2.getMessage());
+
+            Cat cat1 =
+                    (Cat) applicationContext.getBean("cat");
+            Cat cat2 = applicationContext.getBean("cat", Cat.class);
+            System.out.println(bean1 == bean2);
+            System.out.println(cat1 == cat2);
+
+        }
     }
 }
